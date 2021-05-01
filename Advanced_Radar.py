bcb6510fe325c83d34307a2ef30c9c74ddfb6129
Lance Gruber <lancegruber2@gmail.com>
2021-05-01 00:18:13 -0400

    updated config to a template & optimized receiver

--- Advanced_Radar.py
+++ Advanced_Radar.py
@@ -2,18 +2,21 @@ import discord
 from discord.ext.commands import Bot
 from discord import Intents
 
-## for now we default with this
-## in the future, put defaults in a config file that can be changed, so it can be configured in discord with commands and remember changes after restarts
+configFile = open('config.txt','r')
+config = configFile.read().split("\n")
+for i in range(len(config)):
+    config[i] = config[i].split(":")[1]
 categoryCount = 2
 categories = [[],[]]
-write = 837176317425745950
-reads = [837176426435182644,837176452004577281]
-inputName = 'Insight'
-inputName = 'Ecnal'
-admins = ["Ecnal","Ntel"]
-
+adminRole = config[5]
+write = int(config[4])
+reads = config[3].split(",")
+for i in range(len(reads)):
+    reads[i] = int(reads[i])
+inputName = config[2]
+admins = config[1].split(",")
 intents = Intents.all()
-TOKEN = 'ODM3ODY3MjY0MzU0MDI1NDgy.YIyysg.LjwG4jZI5qPAzs_ko0pZ6yvXq4o'
+TOKEN = config[0]
 bot = Bot(intents=intents, command_prefix='?')
 
 @bot.event
@@ -29,7 +32,7 @@ async def receiver(message):
     if message.author.display_name == inputName:
         print("and message is from the target\n")
         for i in range(categoryCount):
-            if message.content in categories[i]:
+            if message.content in categories[i] and reads[i] != message.channel.id:
                 channel = bot.get_channel(write)
                 await channel.send(message.content)
                 for i in range(categoryCount):
@@ -38,20 +41,5 @@ async def receiver(message):
                 break
             if reads[i] == message.channel.id:
                 categories[i].append(message.content)
-        
-@bot.command(name='setup', help = 'Arguments: bot name, number of categories, write channel, read channels')
-async def setup(ctx, name='Insight',categoryCount=2,write=None,*reads):
-    if 'Strategic Command' in ctx.author.roles or ctx.author.name in admins:
-        await context.send('currently broken, sorry')
-        if name == 'Insight' and categoryCount == 2 and write == None:
-            await context.send('you gave me all defaults or nothing at all!')
-        else:
-            if categoryCount <= 1:  
-                await context.send('invalid categoryCount')
-
-@bot.command(name='addadmin', help = "Arguments: username (not nickname) of user to add to admin list")
-async def addadmin(ctx, name):
-    pass
-    
-
+                
 bot.run(TOKEN)
